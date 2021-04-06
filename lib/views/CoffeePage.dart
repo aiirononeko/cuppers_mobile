ad2093dd21a8102aedb7f076652e5e4e00022933
片田凌太 <aiirononeko@gmail.com>
2021-04-06 16:02:18 +0900

    詳細画面表示項目変更

--- lib/views/CoffeePage.dart
+++ lib/views/CoffeePage.dart
@@ -15,9 +15,16 @@ class CoffeePage extends StatelessWidget {
       body: Column(
         children: <Widget>[
           Text(coffeeInfo['cupped_date'].toDate().toString()),
-          Text(coffeeInfo['coffee_name']),
+          Text('${coffeeInfo['coffee_name']} ${coffeeInfo['process']}'),
           Text('Made in ${coffeeInfo['country']}'),
           Text('Score ${coffeeInfo['coffee_score']}'),
+          Text('Sweetness ${coffeeInfo['sweetness']}'),
+          Text('cleancup ${coffeeInfo['cleancup']}'),
+          Text('mousefeel ${coffeeInfo['mousefeel']}'),
+          Text('flavor ${coffeeInfo['flavor']}'),
+          Text('aftertaste ${coffeeInfo['aftertaste']}'),
+          Text('balance ${coffeeInfo['balance']}'),
+          Text('overall ${coffeeInfo['overall']}'),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
