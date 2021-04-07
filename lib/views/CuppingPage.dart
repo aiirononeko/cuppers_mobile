ac37bc75f63120fb795571f9cb11bbeda89232fa
片田凌太 <aiirononeko@gmail.com>
2021-04-07 15:54:46 +0900

    ダミーデータ削除

--- lib/views/CuppingPage.dart
+++ lib/views/CuppingPage.dart
@@ -42,24 +42,6 @@ class _CuppingPageState extends State<CuppingPage> {
   void _slideFlavor(double e) => setState(() {_flavor = e; });
   void _slideOverall(double e) => setState(() {_overall = e; });
 
-  // TODO ダミーデータ
-  final Map<String, dynamic> _dummyCuppingData = {
-    "coffee_name": "イルガチェフ",
-    "country": "エチオピア",
-    "process": "フルウォッシュト",
-    "sweetness": 8,
-    "acidity": 8,
-    "mousefeel": 8,
-    "aftertaste": 8,
-    "cleancup": 8,
-    "balance": 8,
-    "flavor": 8,
-    "overall": 8,
-    "coffee_score": 85,
-    "favorite": false,
-    "cupped_date": Timestamp.fromDate(DateTime.now())
-  };
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -202,20 +184,6 @@ class _CuppingPageState extends State<CuppingPage> {
                 ),
               )
             ],
-            // child: RaisedButton(
-            //   child: const Text('テストデータ投入'),
-            //   color: Colors.blue,
-            //   textColor: Colors.white,
-            //   onPressed: () {
-            //     _setCuppingData(_dummyCuppingData); // TODO ダミー
-            //     Navigator.push(
-            //         context,
-            //         MaterialPageRoute(
-            //             builder: (context) => CoffeePage(coffeeInfo: _dummyCuppingData) // TODO ダミー
-            //         )
-            //     );
-            //   },
-            // ),
           )
       )
     );
