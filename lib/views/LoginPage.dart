a13b829e208463d81a6387ed601993442ca36b28
片田凌太 <aiirononeko@gmail.com>
2021-04-28 13:27:34 +0900

    キーボード入力の際の画面エラーを解消

--- lib/views/LoginPage.dart
+++ lib/views/LoginPage.dart
@@ -35,117 +35,119 @@ class _LoginPageState extends State<LoginPage> {
         elevation: 0.0,
         iconTheme: IconThemeData(color: Colors.black),
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.fromLTRB(30, 130, 30, 50),
-              child: Text(
-                'ログインしましょう',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.fromLTRB(30, 130, 30, 50),
+                child: Text(
+                  'ログインしましょう',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
                 ),
               ),
-            ),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.fromLTRB(30, 0, 30, 30),
-                    child: TextFormField(
-                      enabled: true,
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.email),
-                        hintText: 'example@xxx.com',
-                        labelText: 'Email',
+              Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                          margin: EdgeInsets.fromLTRB(30, 0, 30, 30),
+                          child: TextFormField(
+                            enabled: true,
+                            decoration: const InputDecoration(
+                              icon: Icon(Icons.email),
+                              hintText: 'example@xxx.com',
+                              labelText: 'Email',
+                            ),
+                            onChanged: _handleEmail,
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'メールアドレスを入力してください';
+                              }
+                              if (!RegExp(r'[\w\-\._]+@[\w\-\._]+\.[A-Za-z]+').hasMatch(value)) {
+                                return 'メールアドレスが正しくありません';
+                              }
+                              return null;
+                            },
+                          )
                       ),
-                      onChanged: _handleEmail,
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return 'メールアドレスを入力してください';
-                        }
-                        if (!RegExp(r'[\w\-\._]+@[\w\-\._]+\.[A-Za-z]+').hasMatch(value)) {
-                          return 'メールアドレスが正しくありません';
-                        }
-                        return null;
-                      },
-                    )
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(30, 0, 30, 50),
-                    child: TextFormField(
-                      enabled: true,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.lock),
-                        hintText: 'password',
-                        labelText: 'Password',
+                      Container(
+                          margin: EdgeInsets.fromLTRB(30, 0, 30, 50),
+                          child: TextFormField(
+                            enabled: true,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              icon: Icon(Icons.lock),
+                              hintText: 'password',
+                              labelText: 'Password',
+                            ),
+                            onChanged: _handlePassword,
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'パスワードを入力してください';
+                              }
+                              return null;
+                            },
+                          )
                       ),
-                      onChanged: _handlePassword,
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return 'パスワードを入力してください';
-                        }
-                        return null;
-                      },
-                    )
-                  ),
-                  Container(
-                    width: 300,
-                    height: 50,
-                    margin: EdgeInsets.fromLTRB(30, 0, 30, 30),
-                    child: new ElevatedButton(
-                      child: Text(
-                        'ログイン',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 3
+                      Container(
+                        width: 300,
+                        height: 50,
+                        margin: EdgeInsets.fromLTRB(30, 0, 30, 30),
+                        child: new ElevatedButton(
+                          child: Text(
+                            'ログイン',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 3
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.black87,
+                          ),
+                          onPressed: () {
+                            if (_formKey.currentState.validate()) {
+
+                              // バリデーション成功した場合にログイン処理する
+                              _myFirebaseAuth.loginAndMoveUserPage(
+                                  this._email,
+                                  this._password,
+                                  context
+                              );
+                            }
+                          },
                         ),
                       ),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.black87,
-                      ),
-                      onPressed: () {
-                        if (_formKey.currentState.validate()) {
-
-                          // バリデーション成功した場合にログイン処理する
-                          _myFirebaseAuth.loginAndMoveUserPage(
-                            this._email,
-                            this._password,
-                            context
-                          );
-                        }
-                      },
-                    ),
-                  ),
-                ],
-              )
-            ),
-            Container(
-              child: RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'アカウントをお持ちでない場合 ',
-                      style: TextStyle(color: Colors.black)
-                    ),
-                    TextSpan(
-                      text: '新規登録',
-                      style: TextStyle(color: Colors.blue),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.of(context).pushReplacementNamed('/registration');
-                        }
-                    )
-                  ]
-                )
+                    ],
+                  )
               ),
-            )
-          ],
+              Container(
+                child: RichText(
+                    text: TextSpan(
+                        children: [
+                          TextSpan(
+                              text: 'アカウントをお持ちでない場合 ',
+                              style: TextStyle(color: Colors.black)
+                          ),
+                          TextSpan(
+                              text: '新規登録',
+                              style: TextStyle(color: Colors.blue),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.of(context).pushReplacementNamed('/registration');
+                                }
+                          )
+                        ]
+                    )
+                ),
+              )
+            ],
+          ),
         ),
       )
     );
--- lib/views/RegistrationPage.dart
+++ lib/views/RegistrationPage.dart
@@ -35,172 +35,174 @@ class _RegistrationPageState extends State<RegistrationPage> {
         elevation: 0.0,
         iconTheme: IconThemeData(color: Colors.black),
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.fromLTRB(30, 100, 30, 0),
-              child: Text(
-                'さあ、',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.fromLTRB(30, 100, 30, 0),
+                child: Text(
+                  'さあ、',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
                 ),
               ),
-            ),
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.fromLTRB(30, 0, 30, 50),
-              child: Text(
-                'カッピングを始めよう！',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.fromLTRB(30, 0, 30, 50),
+                child: Text(
+                  'カッピングを始めよう！',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
                 ),
               ),
-            ),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  Container(
-                      margin: EdgeInsets.fromLTRB(30, 0, 30, 30),
-                      child: TextFormField(
-                        enabled: true,
-                        decoration: const InputDecoration(
-                          icon: Icon(Icons.email),
-                          hintText: 'example@xxx.com',
-                          labelText: 'Email',
-                        ),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'メールアドレスを入力してください';
-                          }
-                          if (!RegExp(r'[\w\-\._]+@[\w\-\._]+\.[A-Za-z]+').hasMatch(value)) {
-                            return 'メールアドレスが正しくありません';
-                          }
-                          return null;
-                        },
-                        onChanged: _handleEmail,
-                      )
-                  ),
-                  Container(
-                      margin: EdgeInsets.fromLTRB(30, 0, 30, 50),
-                      child: TextFormField(
-                        enabled: true,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          icon: Icon(Icons.lock),
-                          hintText: 'password',
-                          labelText: 'Password',
-                        ),
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'パスワードを入力してください';
-                          }
-                          if (value.length < 8) {
-                            return 'パスワードは8文字以上で設定してください';
-                          }
-                          return null;
-                        },
-                        onChanged: _handlePassword,
-                      )
-                  ),
-                  Container(
-                      margin: EdgeInsets.fromLTRB(30, 0, 30, 10),
-                      child: RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: '利用規約',
-                              style: TextStyle(color: Colors.blue),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  Navigator.of(context).restorablePushNamed('/login'); // TODO 利用規約ページに遷移するように修正
-                                }
-                            ),
-                            TextSpan(
-                              text: '、 ',
-                              style: TextStyle(color: Colors.black)
-                            ),
-                            TextSpan(
-                              text: 'プライバシーポリシー',
-                              style: TextStyle(color: Colors.blue),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  Navigator.of(context).restorablePushNamed('/login'); // TODO プライバシーポリシーページに遷移するように修正
-                                }
-                            ),
-                            TextSpan(
-                              text: 'に ',
-                              style: TextStyle(color: Colors.black)
-                            )
-                          ]
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                        margin: EdgeInsets.fromLTRB(30, 0, 30, 30),
+                        child: TextFormField(
+                          enabled: true,
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.email),
+                            hintText: 'example@xxx.com',
+                            labelText: 'Email',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'メールアドレスを入力してください';
+                            }
+                            if (!RegExp(r'[\w\-\._]+@[\w\-\._]+\.[A-Za-z]+').hasMatch(value)) {
+                              return 'メールアドレスが正しくありません';
+                            }
+                            return null;
+                          },
+                          onChanged: _handleEmail,
                         )
-                      )
-                  ),
-                  Container(
-                    width: double.infinity,
-                    margin: EdgeInsets.fromLTRB(30, 0, 30, 30),
-                    child: Text(
-                      '同意して新規登録する',
-                      textAlign: TextAlign.center,
                     ),
-                  ),
-                  Container(
-                    width: 300,
-                    height: 50,
-                    margin: EdgeInsets.fromLTRB(30, 0, 30, 30),
-                    child: ElevatedButton(
+                    Container(
+                        margin: EdgeInsets.fromLTRB(30, 0, 30, 50),
+                        child: TextFormField(
+                          enabled: true,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.lock),
+                            hintText: 'password',
+                            labelText: 'Password',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'パスワードを入力してください';
+                            }
+                            if (value.length < 8) {
+                              return 'パスワードは8文字以上で設定してください';
+                            }
+                            return null;
+                          },
+                          onChanged: _handlePassword,
+                        )
+                    ),
+                    Container(
+                        margin: EdgeInsets.fromLTRB(30, 0, 30, 10),
+                        child: RichText(
+                            text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                      text: '利用規約',
+                                      style: TextStyle(color: Colors.blue),
+                                      recognizer: TapGestureRecognizer()
+                                        ..onTap = () {
+                                          Navigator.of(context).restorablePushNamed('/login'); // TODO 利用規約ページに遷移するように修正
+                                        }
+                                  ),
+                                  TextSpan(
+                                      text: '、 ',
+                                      style: TextStyle(color: Colors.black)
+                                  ),
+                                  TextSpan(
+                                      text: 'プライバシーポリシー',
+                                      style: TextStyle(color: Colors.blue),
+                                      recognizer: TapGestureRecognizer()
+                                        ..onTap = () {
+                                          Navigator.of(context).restorablePushNamed('/login'); // TODO プライバシーポリシーページに遷移するように修正
+                                        }
+                                  ),
+                                  TextSpan(
+                                      text: 'に ',
+                                      style: TextStyle(color: Colors.black)
+                                  )
+                                ]
+                            )
+                        )
+                    ),
+                    Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.fromLTRB(30, 0, 30, 30),
                       child: Text(
-                        '新規登録',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 4
-                        ),
+                        '同意して新規登録する',
+                        textAlign: TextAlign.center,
                       ),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.black87,
-                      ),
-                      onPressed: () async {
-
-                        if (_formKey.currentState.validate()) {
-
-                          // ユーザー登録・ログイン処理
-                          await _myFirebaseAuth.createUserAndLogin(
-                            this._email,
-                            this._password,
-                            context
-                          );
-                        }
-                      },
                     ),
-                  ),
-                ],
+                    Container(
+                      width: 300,
+                      height: 50,
+                      margin: EdgeInsets.fromLTRB(30, 0, 30, 30),
+                      child: ElevatedButton(
+                        child: Text(
+                          '新規登録',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 4
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.black87,
+                        ),
+                        onPressed: () async {
+
+                          if (_formKey.currentState.validate()) {
+
+                            // ユーザー登録・ログイン処理
+                            await _myFirebaseAuth.createUserAndLogin(
+                                this._email,
+                                this._password,
+                                context
+                            );
+                          }
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'すでにアカウントをお持ちの場合 ',
-                    style: TextStyle(color: Colors.black)
-                  ),
-                  TextSpan(
-                    text: 'ログイン',
-                    style: TextStyle(color: Colors.blue),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Navigator.of(context).restorablePushNamed('/login');
-                      }
+              RichText(
+                  text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: 'すでにアカウントをお持ちの場合 ',
+                            style: TextStyle(color: Colors.black)
+                        ),
+                        TextSpan(
+                            text: 'ログイン',
+                            style: TextStyle(color: Colors.blue),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.of(context).restorablePushNamed('/login');
+                              }
+                        )
+                      ]
                   )
-                ]
               )
-            )
-          ],
+            ],
+          ),
         ),
       )
     );
